b40bc19f12d23e967eb379628456b904776b06b5
theHive <thehive@example.com>
2019-07-24 16:10:30 -0300

    Elimino match_ip.py

--- webhooks/funciones/match_ip.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-from parametros import *
-
-#recibe un json y matchea si alguna ip es interna
-
-def match_ip(data): # New case observable created
-   r = requests.post(hookURL, data, headers=headers)
-   "Response: " + str(r.status_code) + "," + str(r.reason) 
