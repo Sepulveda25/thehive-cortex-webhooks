38e2ba6eb65e5bc6e46bda6de31b2c29377418db
theHive <thehive@example.com>
2019-07-31 17:54:38 -0300

    Comento cosas en theHiveWebhook.py y modifico web_application_attack.py

--- webhooks/funciones/web_application_attack.py
+++ webhooks/funciones/web_application_attack.py
@@ -2,25 +2,46 @@
 
 from parametros import *
 
+
 def web_application_attack(data):
+    print("Busco ID del responder")
 
-    print("Llamo al respoder correspondiente")
+    responder_name = 'MailsExcel'
+    id_responder = 0
 
-    #url = 'http://172.16.81.110:9001/api/responder/_search?range=all'
-    url = 'http://172.16.81.110:9001/api/responder/29165d3c9329be9dd8a439fc1d4a1d66/run'
+    # cortexURL definido en paramtros.py
+    search_url = cortexURL + '/api/responder/_search?range=all'
+
+    search_headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer baDVP07GJj7uOcWEp7sSpU+oJ42/GJKr"
+    }
+
+    search_data = {"query": {"dataTypeList": "thehive:alert"}}
+
+    r = requests.post(search_url, data=json.dumps(search_data), headers=search_headers)
+    json_response = r.json()
+
+    # print(json_response)
+    # Busco adentro del array el coincide con el responder con el opeardor "in"
+    for i in json_response:
+        if (responder_name in i['name']):
+            id_responder = i['id']
+            break
+
+    print(id_responder)
+
+    # url = 'http://172.16.81.110:9001/api/responder/29165d3c9329be9dd8a439fc1d4a1d66/run'
 
     headers = {
-                "Content-Type": "application/json",
-                "Authorization": "Bearer baDVP07GJj7uOcWEp7sSpU+oJ42/GJKr"
-               }
+        "Content-Type": "application/json",
+        "Authorization": "Bearer baDVP07GJj7uOcWEp7sSpU+oJ42/GJKr"
+    }
 
-    print(data)
+# print(data)
 
-    #datos = {"label":"[SecurityOnion:57ba6f] New IDS Alert! -- SURICATA SMTP invalid pipelined sequence ","data":{"severity":"3","date":"1564081498000","_routing":"f484b8ae64474689352604ccb9985f54","customFields":{},"caseTemplate":"null","_type":"alert","description":"[1:2220004:1] SURICATA SMTP invalid pipelined sequence [Classification: Generic Protocol Command Decode] [Priority: 3]: <sonion-virtual-machine-ens192> {TCP} 200.16.22.73:60916 -> 200.16.22.1:25","lastSyncDate":"1564081494991","source":"SecurityOnion","title":"New IDS Alert! -- SURICATA SMTP invalid pipelined sequence ","type":"external","follow":"true","tags":["elastalert, SecurityOnion, {match[category]}"],"createdAt":"1564081494990","_parent":"null","createdBy":"eco","tlp":"3","_id":"f484b8ae64474689352604ccb9985f54","id":"f484b8ae64474689352604ccb9985f54","sourceRef":"57ba6f","_version":"1","artifacts":[{"data":"200.16.22.73","dataType":"source_ip","message":"null","tags":[],"tlp":"2"},{"data":"200.16.22.1","dataType":"destination_ip","message":"null","tags":[],"tlp":"2"},{"data":"60916","dataType":"source_port","message":"null","tags":[],"tlp":"2"},{"data":"25","dataType":"destination_port","message":"null","tags":[],"tlp":"2"},{"data":"sonion-virtual-machine-ens192","dataType":"interface","message":"null","tags":[],"tlp":"2"},{"data":"2019-07-25T18:59:57.732Z","dataType":"timestamp","message":"null","tags":[],"tlp":"2"},{"data":"snort","dataType":"event_type","message":"null","tags":[],"tlp":"2"}],"status":"New"},"dataType":"thehive:alert","tlp":"2","pap":"2","message":"","parameters":{"user":"thehive"}}
+# datos = {"label":"[SecurityOnion:57ba6f] New IDS Alert! -- SURICATA SMTP invalid pipelined sequence ","data":{"severity":"3","date":"1564081498000","_routing":"f484b8ae64474689352604ccb9985f54","customFields":{},"caseTemplate":"null","_type":"alert","description":"[1:2220004:1] SURICATA SMTP invalid pipelined sequence [Classification: Generic Protocol Command Decode] [Priority: 3]: <sonion-virtual-machine-ens192> {TCP} 200.16.22.73:60916 -> 200.16.22.1:25","lastSyncDate":"1564081494991","source":"SecurityOnion","title":"New IDS Alert! -- SURICATA SMTP invalid pipelined sequence ","type":"external","follow":"true","tags":["elastalert, SecurityOnion, {match[category]}"],"createdAt":"1564081494990","_parent":"null","createdBy":"eco","tlp":"3","_id":"f484b8ae64474689352604ccb9985f54","id":"f484b8ae64474689352604ccb9985f54","sourceRef":"57ba6f","_version":"1","artifacts":[{"data":"200.16.22.73","dataType":"source_ip","message":"null","tags":[],"tlp":"2"},{"data":"200.16.22.1","dataType":"destination_ip","message":"null","tags":[],"tlp":"2"},{"data":"60916","dataType":"source_port","message":"null","tags":[],"tlp":"2"},{"data":"25","dataType":"destination_port","message":"null","tags":[],"tlp":"2"},{"data":"sonion-virtual-machine-ens192","dataType":"interface","message":"null","tags":[],"tlp":"2"},{"data":"2019-07-25T18:59:57.732Z","dataType":"timestamp","message":"null","tags":[],"tlp":"2"},{"data":"snort","dataType":"event_type","message":"null","tags":[],"tlp":"2"}],"status":"New"},"dataType":"thehive:alert","tlp":"2","pap":"2","message":"","parameters":{"user":"thehive"}}
 
-    r = requests.post(url, data=json.dumps(data), headers=headers)
+# r = requests.post(url, data=json.dumps(data), headers=headers)
 
-    print(r)
-
-
-
+# print(r)
